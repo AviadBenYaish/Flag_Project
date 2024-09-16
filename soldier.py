3c32cf983a78e25de8600b951e46c6bea6a4d8db
class testing <yharazak@gmail.com>
2024-09-16 14:37:05 +0300

    in

--- soldier.py
+++ soldier.py
@@ -9,9 +9,9 @@ def moving_down(soldier_position):
     soldier_position = list(soldier_position)
     soldier_position[1] += 1
     return tuple(soldier_position)
-def moving_up(soldier_position):
+def moving_left(soldier_position):
     soldier_position = list(soldier_position)
-    soldier_position[0] += 1
+    soldier_position[0] -= 1
     return tuple(soldier_position)
 
 
